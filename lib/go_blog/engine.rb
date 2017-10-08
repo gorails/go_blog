69d983a4857eeea0dc3be5b2916caeba5c15bf0c
João Carlos Ottobboni <jcottobboni@gmail.com>
2017-10-08 17:18:29 -0300

    test

--- lib/go_blog/engine.rb
+++ lib/go_blog/engine.rb
@@ -1,5 +1,9 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
 module GoBlog
   class Engine < ::Rails::Engine
+    config.autoload_paths += %W(#{config.root}/lib/modules)
     initializer :append_migrations do |app|
       # This prevents migrations from being loaded twice from the inside of the
       # gem itself (dummy test app)
