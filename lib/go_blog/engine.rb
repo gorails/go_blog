88c9f100cabcd3e7d5c1becd5750f8d16fe49b0c
João Carlos Ottobboni <jcottobboni@gmail.com>
2017-10-09 22:30:34 -0300

    update blog

--- lib/go_blog/engine.rb
+++ lib/go_blog/engine.rb
@@ -1,7 +1,7 @@
-
 require 'rails/all'
 module GoBlog
   class Engine < ::Rails::Engine
+
     initializer :append_migrations do |app|
       # This prevents migrations from being loaded twice from the inside of the
       # gem itself (dummy test app)
@@ -11,5 +11,14 @@ module GoBlog
         end
       end
     end
+
+    config.i18n.default_locale = :'pt-BR'
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.time_zone = 'Brasilia'
+    config.i18n.load_path += Dir[config.root.join('config', 'locales','**', '*.{rb,yml}').to_s]
+    config.encoding = "utf-8"
+
+
   end
+
 end
